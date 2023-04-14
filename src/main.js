23b70f6ac74d34c18c7078f7ff574bf096f60b0d
keyshid <keyshids@gmail.com>
2023-04-15 01:51:11 +0430

    done

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import VueThreeSixty from 'vue-360'
+import 'vue-360/dist/css/style.css'
+Vue.use(VueThreeSixty)
 Vue.config.productionTip = false
 
 new Vue({
